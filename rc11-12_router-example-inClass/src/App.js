99dce430afc1badf9d80d1a6e012c1f72efd976c
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-17 21:42:30 +0100

    instructors component changed with people

--- rc11-12_router-example-inClass/src/App.js
+++ rc11-12_router-example-inClass/src/App.js
@@ -2,22 +2,25 @@ import Footer from "./components/Footer";
 import Nav from "./components/Nav";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
-import Instructors from "./pages/Instructors";
 import NotFound from "./pages/NotFound";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import People from "./pages/People";
+import { Routes, Route } from "react-router-dom";
+import Paths from "./pages/Paths";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/instructors" element={<Instructors />} />
+        <Route path="/people" element={<People />} />
+        <Route path="/paths" element={<Paths />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
+
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
