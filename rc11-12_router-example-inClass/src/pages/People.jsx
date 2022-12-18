3390bb5cd5bfdb3808bda197cd2801d1a66f8f38
Yasin Yavuz <barbaros1675@gmail.com>
2022-12-18 20:57:21 +0100

    people buttons added

--- rc11-12_router-example-inClass/src/pages/People.jsx
+++ rc11-12_router-example-inClass/src/pages/People.jsx
@@ -26,12 +26,12 @@ const People = () => {
               key={id}
               className=" col-sm-12 col-md-6 col-lg-4"
               type="button"
-              // onClick={() => navigate(`/people/${id}`, { state: person })}
+              onClick={() => navigate(`/people/${id}`, { state: person })}
               //! absolute path
               // onClick={() => navigate(`/people/${id}`)}
 
               //! relative path
-              onClick={() => navigate(`${id}`)}
+              // onClick={() => navigate(`${id}`)}
             >
               <img className="rounded" src={avatar} alt="img" />
               <h6>
--- rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
 
 const PersonDetail = () => {
-  return <div>PersonDetail</div>;
+  const navigate = useNavigate();
+  const { id } = useParams();
+  console.log(id);
+
+  const { state: person } = useLocation();
+  console.log(person);
+
+  return (
+    <div className="container text-center">
+      <h3>
+        {person.first_name} {person.last_name}
+      </h3>
+      <img className="rounded" src={person.avatar} alt="" />
+      <p> {person.email} </p>
+      <div>
+        <button onClick={() => navigate("/")} className="btn btn-success me-2">
+          Home
+        </button>
+        <button onClick={() => navigate(-1)} className="btn btn-warning">
+          Go Back
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PersonDetail;
